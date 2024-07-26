1903901cc5ebc522d177bc394fdfb3e1d0311596
DinkelTwinkel <128545374+DinkelTwinkel@users.noreply.github.com>
2024-07-26 17:35:54 +0100

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -8,7 +8,7 @@ module.exports = {
     .addUserOption(option =>
 			option
 				.setName('target')
-				.setDescription('target for murdur')
+				.setDescription('target for murdur, costs 1000 mayo')
 				.setRequired(true)),
 
     async execute(interaction, client) {
@@ -17,16 +17,18 @@ module.exports = {
 
       const findPouch = await Point.findOne({ userId: interaction.member.id });
       //const targetPouch = await Point.findOne({ userId: interaction.options.getUser('target').id });
+      const target = interaction.options.getMember('target');
 
       const amountToGive = 1000;
 
       if (findPouch.points >= amountToGive) {
 
+          interaction.reply (`${interaction.options.getUser('target')} has been killed.`);
 
-          interaction.options.getUser('target').roles.set(['1172197535922798644']).catch((err) => {});
+          target.roles.set(['1172197535922798644']).catch((err) => {});
           findPouch.points -= amountToGive;
 
-          interaction.reply (`${interaction.options.getUser('target')} has been killed.`);
+          
 
         await findPouch.save();
 
